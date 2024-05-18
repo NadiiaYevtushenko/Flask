2078f785a21541c920c5830e9f5236908a5bb3d1
NadiiaYevtushenko <nedilja90@gmail.com>
2024-05-18 06:49:27 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name,id):  # put application's code here
+    return "User page: " + name + " - " + str(id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
